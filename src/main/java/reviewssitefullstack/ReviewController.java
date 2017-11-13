7c9cdf80e65bf404c340984d20d90a888a1a326a
yesharge <yeshargeabrha@gmail.com>
2017-11-13 12:56:25 -0500

    Fixed spacing in reviews

--- src/main/java/reviewssitefullstack/ReviewController.java
+++ src/main/java/reviewssitefullstack/ReviewController.java
@@ -1,4 +1,5 @@
 package reviewssitefullstack;
+
 import javax.annotation.Resource;
 
 import org.springframework.data.domain.Sort;
@@ -9,57 +10,60 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReviewController {
-	
-		@Resource
-		CategoryRepository categoryRepo;
-		
-		@Resource
-		ReviewRepository reviewRepo;
 
-		@Resource
-		SubscriberRepository subscriberRepo = new SubscriberRepository();
-		@Resource
-		TagRepository tagRepo;
-		
-		@RequestMapping("/reviews")
-		public String getAllBooks (Model model) {
-			model.addAttribute("reviews", reviewRepo.findAll());
-			return "reviews";
-		}
-		
-		@RequestMapping("/categories")
-		public String getAllAuthors (Model model) {
-			model.addAttribute("categories", categoryRepo.findAll());
-			return "categories";
-		}
-		@RequestMapping("/review")
-		public String getOneAuthor(@RequestParam Long id, Model model) {
-			model.addAttribute("review", reviewRepo.findOne(id));
-			return "review";
-		}
-		@RequestMapping("/category")
-		public String getBooksByGenre(@RequestParam Long id, Model model) {
-			model.addAttribute("category", categoryRepo.findOne(id));
-			return "category";
-		}
-		@RequestMapping("/add-subscriber")
-		public String addSubscriber(String name, String email) {
-			Subscriber newSubscriber = new Subscriber(name,email);
-			subscriberRepo.addSubscriber(newSubscriber);
-			
-			return "redirect:/categories";
-		}
-		@RequestMapping("/tag")
-		public String findTag(@RequestParam Long id, Model model) {
-			model.addAttribute("tag",tagRepo.findOne(id));
-				return "tag";
-		}
-		@RequestMapping("/tags")
-		public String allTags(Model model) {
-			model.addAttribute("tags", tagRepo.findAll());
-			return "tags";
-		}
-		
-		
-		
+	@Resource
+	CategoryRepository categoryRepo;
+
+	@Resource
+	ReviewRepository reviewRepo;
+
+	@Resource
+	SubscriberRepository subscriberRepo = new SubscriberRepository();
+	@Resource
+	TagRepository tagRepo;
+
+	@RequestMapping("/reviews")
+	public String getAllBooks(Model model) {
+		model.addAttribute("reviews", reviewRepo.findAll());
+		return "reviews";
 	}
+
+	@RequestMapping("/categories")
+	public String getAllAuthors(Model model) {
+		model.addAttribute("categories", categoryRepo.findAll());
+		return "categories";
+	}
+
+	@RequestMapping("/review")
+	public String getOneAuthor(@RequestParam Long id, Model model) {
+		model.addAttribute("review", reviewRepo.findOne(id));
+		return "review";
+	}
+
+	@RequestMapping("/category")
+	public String getBooksByGenre(@RequestParam Long id, Model model) {
+		model.addAttribute("category", categoryRepo.findOne(id));
+		return "category";
+	}
+
+	@RequestMapping("/add-subscriber")
+	public String addSubscriber(String name, String email) {
+		Subscriber newSubscriber = new Subscriber(name, email);
+		subscriberRepo.addSubscriber(newSubscriber);
+
+		return "redirect:/categories";
+	}
+
+	@RequestMapping("/tag")
+	public String findTag(@RequestParam Long id, Model model) {
+		model.addAttribute("tag", tagRepo.findOne(id));
+		return "tag";
+	}
+
+	@RequestMapping("/tags")
+	public String allTags(Model model) {
+		model.addAttribute("tags", tagRepo.findAll());
+		return "tags";
+	}
+
+}
--- src/main/java/reviewssitefullstack/ReviewPopulator.java
+++ src/main/java/reviewssitefullstack/ReviewPopulator.java
@@ -49,19 +49,20 @@ public class ReviewPopulator implements CommandLineRunner {
 				"/images/tarte.jpg", tarte, face));
 		reviewRepo.save(new Review("Eye Bright As Day", "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
 				"/images/tarte2.jpg", tarte, face));
-		reviewRepo.save(new Review("Highlight Seen From Space ",
-				"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "/images/becca2.jpg", becca, face,highlight));
+		reviewRepo.save(
+				new Review("Highlight Seen From Space ", "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+						"/images/becca2.jpg", becca, face, highlight));
 		reviewRepo.save(new Review("Eye Shadow For the Lazy",
 				"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "/images/lorac.jpg", lorac, face));
 		reviewRepo.save(new Review("Eye Shadow For the Mundane",
 				"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "/images/lorac2.jpg", lorac, face));
 		reviewRepo
 				.save(new Review("Highlight of the Night", "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-						"/images/trophywife.jpg", fentyBeauty, face,highlight));
+						"/images/trophywife.jpg", fentyBeauty, face, highlight));
 		reviewRepo.save(new Review("Dab on these Haters", "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
 				"/images/fentydab.jpg", fentyBeauty, face));
 		reviewRepo.save(new Review("Steal the Spotlight", "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-				"/images/fentysteal.jpg", fentyBeauty,lips));
+				"/images/fentysteal.jpg", fentyBeauty, lips));
 		reviewRepo.save(new Review("Bold Lips", "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
 				"/images/narsbold.jpg", nars, lips));
 		reviewRepo.save(new Review("Warm Lips", "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
